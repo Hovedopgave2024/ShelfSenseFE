6465adc4a3d5339524baa1dfdbd7b69495c1a397
Frederik Wessel <frederikglimsager@hotmail.com>
2024-11-21 10:59:34 +0100

    edit buttons for components

--- shelfsense/src/components/components/ComponentsTableRow.jsx
+++ shelfsense/src/components/components/ComponentsTableRow.jsx
@@ -1,4 +1,4 @@
-import { TableRow, TableCell } from '@mui/material';
+import { TableRow, TableCell, Button } from '@mui/material';
 
 const ComponentsTableRow = ({ component }) => (
     <TableRow hover key={component.id}>
@@ -7,6 +7,15 @@ const ComponentsTableRow = ({ component }) => (
         <TableCell align="left">{component.price}</TableCell>
         <TableCell align="left">{component.type}</TableCell>
         <TableCell align="left">{component.quantity}</TableCell>
+            <TableCell align="left">
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        onClick={() => onEdit(component)} // Callback for editing
+                    >
+                            Edit
+                    </Button>
+            </TableCell>
     </TableRow>
 );
 
