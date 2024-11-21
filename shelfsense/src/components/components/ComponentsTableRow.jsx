c9e88069a05177521ebd98209719e4119aee56b0
Frederik Wessel <frederikglimsager@hotmail.com>
2024-11-21 10:39:45 +0100

    added manufacturer part to the components table

--- shelfsense/src/components/components/ComponentsTableRow.jsx
+++ shelfsense/src/components/components/ComponentsTableRow.jsx
@@ -3,6 +3,7 @@ import { TableRow, TableCell } from '@mui/material';
 const ComponentsTableRow = ({ component }) => (
     <TableRow hover key={component.id}>
         <TableCell align="left">{component.name}</TableCell>
+        <TableCell align="left">{component.manufacturerPart}</TableCell>
         <TableCell align="left">{component.price}</TableCell>
         <TableCell align="left">{component.type}</TableCell>
         <TableCell align="left">{component.quantity}</TableCell>
