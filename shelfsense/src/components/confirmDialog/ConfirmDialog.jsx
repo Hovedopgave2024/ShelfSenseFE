1e67b9400bfb095dec597d660eb1c844c2584a70
Frederik Wessel <frederikglimsager@hotmail.com>
2024-12-04 12:16:24 +0100

    adjusted confirmdialog modal to the darkmode

--- shelfsense/src/components/confirmDialog/ConfirmDialog.jsx
+++ shelfsense/src/components/confirmDialog/ConfirmDialog.jsx
@@ -4,7 +4,7 @@ import {
     DialogContent,
     DialogActions,
     Button,
-    Typography, Box,
+    Typography, Box, useTheme,
 } from "@mui/material";
 import Divider from "@mui/material/Divider";
 
@@ -19,6 +19,8 @@ const ConfirmDialog = ({
                            declineText = "Decline",
                            color = "error",
                        }) => {
+    const theme = useTheme(); // Access the current theme
+
     return (
         <Dialog open={open} onClose={onClose} aria-labelledby="dialog-title" PaperProps={{
             sx: {
@@ -26,7 +28,7 @@ const ConfirmDialog = ({
                 boxShadow: "10px 15px 25px rgba(0, 0, 0, 0.3)",
             },
         }}>
-            <Box sx={{ backgroundColor: "#e3f2fd", padding: 1 }}>
+            <Box sx={{ backgroundColor: theme.palette.background.paper, padding: 1 }}>
                 <DialogTitle id="dialog-title">
                     <Typography
                         variant="h5"
@@ -35,8 +37,8 @@ const ConfirmDialog = ({
                     </Typography>
                 </DialogTitle>
             </Box>
-            <Divider sx={{ backgroundColor: "#bbdefb" }} />
-            <DialogContent sx={{ backgroundColor: "#f1f8ff", padding: 3 }}>
+            <Divider sx={{ backgroundColor: theme.palette.divider, }} />
+            <DialogContent sx={{ backgroundColor: theme.palette.background.default, padding: 3 }}>
                 <Typography variant="body1" >
                     {text}
                 </Typography>
