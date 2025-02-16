cc8cfa0de77c1bea3d9c60c67450c80365fbbf2d
PedroLorenzen <christianplorenzen@outlook.com>
2025-02-16 12:47:55 +0100

    use the salesorder card instead and add functionality to select products

--- shelfsense/src/components/salesOrders/SalesOrdersCreateCard.jsx
+++ shelfsense/src/components/salesOrders/SalesOrdersCreateCard.jsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import Grid from '@mui/material/Grid2';
 import dayjs from 'dayjs';
-import {TextField, Card, CardContent, CardHeader, Button} from "@mui/material";
+import {TextField, Card, CardContent, CardHeader, Button, Typography, FormControl, IconButton} from "@mui/material";
 import useSnackbarStore from "../../stores/useSnackbarStore.js";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -13,6 +13,7 @@ import {createSalesOrder} from "../../services/salesOrder/createSalesOrder.js";
 import useSalesOrdersStore from "../../stores/useSalesOrdersStore.js";
 import useComponentsStore from "../../stores/useComponentsStore.js";
 import calculateStatus from "../../util/component/calculateStockStatus.js";
+import {Add, Remove} from "@mui/icons-material";
 
 
 const SalesOrdersCreateCard = () => {
@@ -63,15 +64,22 @@ const SalesOrdersCreateCard = () => {
         }
     };
 
-    const handleAddProduct = (event, newValue) => {
-        if (newValue && !selectedProducts.some(p => p.productId === newValue.id)) {
-            setSelectedProducts([...selectedProducts, {
-                productId: newValue.id,
-                name: newValue.name,
-                checked: true,
-                quantity: 1  // Default quantity to 1
-            }]);
-        }
+    const handleProductChange = (index, field, value) => {
+        const updatedProducts = [...selectedProducts];
+        updatedProducts[index] = {
+            ...updatedProducts[index],
+            [field]: value };
+        setSelectedProducts(updatedProducts);
+    };
+
+    const handleAddProduct = () => {
+        setSelectedProducts([...selectedProducts, { productId: '', quantity: '' }]);
+    };
+
+    const handleRemoveProduct = (index) => {
+        const updatedProducts = [...selectedProducts];
+        updatedProducts.splice(index, 1);
+        setSelectedProducts(updatedProducts);
     };
 
     const validateForm = () => {
@@ -98,9 +106,14 @@ const SalesOrdersCreateCard = () => {
             }
         });
 
-        if (selectedProducts.length === 0) {
-            newErrors.products = 'At least one product must be selected';
-        }
+        selectedProducts.forEach((product, index) => {
+            if (!product.productId) {
+                newErrors[`productId_${index}`] = "Product is required.";
+            }
+            if (!product.quantity || isNaN(product.quantity) || product.quantity <= 0) {
+                newErrors[`quantity_${index}`] = "Valid quantity is required.";
+            }
+        });
 
         setErrors(newErrors);
         showSnackbar('warning', 'Please fill out all fields correctly and try again');
@@ -111,40 +124,43 @@ const SalesOrdersCreateCard = () => {
     const handleSubmit = async () => {
         if (!validateForm()) return;
 
-        const result = await createSalesOrder(formData);
+        const salesOrderData = {
+            price: formData.price,
+            createdDate: formData.createdDate,
+            salesOrderProducts: selectedProducts.map((product) => ({
+                productId: product.productId,
+                quantity: product.quantity,
+            })),
+        };
+
+        const result = await createSalesOrder(salesOrderData);
 
         if (!result) {
-            showSnackbar('error', 'Error: Sales order was not created. Please try again or contact Support');
+            showSnackbar("error", "Error: Sales order was not created.");
             return;
         }
 
-        const newSalesOrder = {
-            id: result.id,
-            productId: result.productId,
-            productName: result.productName,
-            quantity: result.quantity,
-            price: result.price,
-            createdDate: result.createdDate,
-        };
+        addSalesOrder(result);
 
-        addSalesOrder(newSalesOrder);
+        result.salesOrderProducts?.forEach((product) => {
 
-        const product = products.find((product) => product.id === newSalesOrder.productId);
+            const selectedProduct = products.find((p) => p.id === product.id);
 
-        if (product && product.productComponentList.length !== 0) {
-            product.productComponentList.forEach((pc) => {
-                const component = components.find((component) => component.id === pc.componentId);
-                const requiredQuantity = parseInt(pc.quantity) * parseInt(newSalesOrder.quantity);
-                const updatedStock = parseInt(component.stock) - requiredQuantity;
+            if (selectedProduct && selectedProduct.productComponentList.length !== 0) {
+                selectedProduct.productComponentList.forEach((pc) => {
+                    const component = components.find((component) => component.id === pc.componentId);
+                    const requiredQuantity = parseInt(pc.quantity) * parseInt(product.quantity);
+                    const updatedStock = parseInt(component.stock) - requiredQuantity;
 
-                updateComponent({
-                    ...component,
-                    id: component.id,
-                    stock: Number(updatedStock),
-                    stockStatus: calculateStatus(Number(updatedStock), Number(component.safetyStock), Number(component.safetyStockRop))
+                    updateComponent({
+                        ...component,
+                        id: component.id,
+                        stock: Number(updatedStock),
+                        stockStatus: calculateStatus(Number(updatedStock), Number(component.safetyStock), Number(component.safetyStockRop))
+                    });
                 });
-            });
-        }
+            }
+        });
 
         showSnackbar('success', 'Sales order created successfully');
         setFormData(initialFormData);
@@ -153,133 +169,162 @@ const SalesOrdersCreateCard = () => {
 
     // Reset form data and errors when the modal opens
     useEffect(() => {
-            setFormData(initialFormData);
-            setErrors({});
+        setFormData(initialFormData);
+        setErrors({});
     }, []);
 
     return (
-        <Card sx={{
-            maxWidth: { lg: 400, md: '100%' },
-            width: '100%',
-            pt: 2,
-            px: 2,
-            borderRadius: 5,
-            mt: { lg: 9, md: 5 },
-            mb: 2,
-        }}>
-            <CardHeader
-                title="Create a New Sales Order"
-                sx={{ textAlign: 'center' }}
-            />
+        <Card
+            sx={{
+                maxWidth: { lg: 400, md: '100%' },
+                width: '100%',
+                pt: 2,
+                px: 2,
+                borderRadius: 5,
+                mt: { lg: 9, md: 5 },
+                mb: 2,
+            }}
+        >
+            <CardHeader title="Create a New Sales Order" sx={{ textAlign: 'center' }} />
             <CardContent>
+                <Grid
+                    container
+                    spacing={2}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    sx={{ pb: 3 }}
+                >
                     <Grid
                         container
                         spacing={2}
                         sx={{
-                           display: 'flex',
-                           justifyContent: 'center',
-                           alignItems: 'center',
-                            height: '100%',
-                           pb: 3,
-                       }}>
-                        <Grid xs={12} lg={3} >
-                            <Autocomplete sx={{ width: 195 }}
-                                options={products}
-                                getOptionLabel={(option) => option.name || ''}
-                                value={products.find((prod) => prod.id === formData.productId) || null}
-                                onChange={(e, newValue) => {
-                                    setFormData((prevData) => ({
-                                        ...prevData,
-                                        productId: newValue?.id || '',
-                                        productName: newValue?.name || '',
-                                    }));
-                                    if (errors.productId) {
-                                        setErrors((prevErrors) => ({
-                                            ...prevErrors,
-                                            productId: null,
-                                        }));
-                                    }
-                                }}
-                                onInputChange={(e, newInputValue) => {
-                                    setFormData((prevData) => ({
-                                        ...prevData,
-                                        productId: newInputValue || '',
-                                    }));
-                                    if (errors.productId) {
-                                        setErrors((prevErrors) => ({
-                                            ...prevErrors,
-                                            productId: null,
-                                        }));
-                                    }
-                                }}
-                                renderInput={(params) => (
-                                    <TextField
-                                        {...params}
-                                        label="Product ID"
-                                        name="productId"
-                                        variant="outlined"
-                                        error={!!errors.productId}
-                                        helperText={errors.productId || ''}
-                                    />
-                                )}
-                            />
-                        </Grid>
-                        <Grid xs={12} lg={3} >
-                            <TextField
-                                    label={requiredFields.includes('quantity')
-                                        ? `quantity *`
-                                        : 'quantity'}
-                                    name='quantity'
-                                    variant="outlined"
-                                    sx={{ width: 195 }}
-                                    value={formData.quantity}
-                                    onChange={handleChange}
-                                    error={!!errors.quantity}
-                                    helperText={errors.quantity || ''}
-                                    type='number'
-                            />
-                        </Grid>
-                        <Grid xs={12} lg={3}>
-                            <TextField
-                                label={requiredFields.includes('price')
-                                    ? `price *`
-                                    : 'price'}
-                                name='price'
-                                variant="outlined"
-                                sx={{ width: 195 }}
-                                value={formData.price}
-                                onChange={handleChange}
-                                error={!!errors['price']}
-                                helperText={errors['price'] || ''}
-                                type='number'
-                            />
-                        </Grid>
-                        <Grid xs={12} lg={3}>
-                            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="da">
-                                <DatePicker
-                                    sx={{ width: 195 }}
-                                    label="Order Date"
-                                    value={dayjs(formData.createdDate)}
-                                    onChange={handleDateChange}
-                                    slotProps={{
-                                        textField: {
-                                            variant: 'outlined',
-                                            error: !!errors.createdDate,
-                                            helperText: errors.createdDate || '',
-                                        },
-                                    }}
-                                />
-                            </LocalizationProvider>
-                        </Grid>
+                        overflowY: 'auto',
+                        maxHeight: '46vh',
+                        mb: 1
+                    }}
+                    >
+                    {/* Price Input */}
+                    <Grid item sx={{width: "100%"}}>
+                        <TextField
+                            label={requiredFields.includes('price') ? `Price *` : 'Price'}
+                            name="price"
+                            variant="outlined"
+                            fullWidth
+                            value={formData.price}
+                            onChange={handleChange}
+                            error={!!errors['price']}
+                            helperText={errors['price'] || ''}
+                            type="number"
+                        />
                     </Grid>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    onClick={handleSubmit}
-                >
-                    Save Sales Order
-                </Button>
+
+                    {/* Date Picker */}
+                    <Grid item sx={{width: "100%"}}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="da">
+                            <DatePicker
+                                label="Order Date"
+                                value={dayjs(formData.createdDate)}
+                                onChange={handleDateChange}
+                                slotProps={{
+                                    textField: {
+                                        variant: 'outlined',
+                                        error: !!errors.createdDate,
+                                        helperText: errors.createdDate || '',
+                                        fullWidth: true,
+                                    },
+                                }}
+                            />
+                        </LocalizationProvider>
+                    </Grid>
+
+                    {/* Selected Products Section */}
+                    <Grid item xs={12} sx={{width: "100%", textAlign: 'center'}}>
+                        <Typography variant="subtitle1">Add Products</Typography>
+                    </Grid>
+
+                    {selectedProducts.map((product, index) => (
+                        <Grid
+                            container
+                            key={index}
+                            direction="row"
+                        >
+                            {/* Product Selection */}
+                            <Grid item sx={{ width: '100%' }}>
+                                <FormControl fullWidth>
+                                    <Autocomplete
+                                        options={products.filter(
+                                            (p) =>
+                                                !selectedProducts.some(
+                                                    (selected) =>
+                                                        selected.productId === p.id &&
+                                                        selected.productId !== product.productId
+                                                )
+                                        )}
+                                        getOptionLabel={(option) => option.name}
+                                        value={
+                                            product.productId
+                                                ? products.find((p) => p.id === product.productId) || null
+                                                : null
+                                        }
+                                        isOptionEqualToValue={(option, value) => option.id === value.id}
+                                        onChange={(e, newValue) =>
+                                            handleProductChange(index, 'productId', newValue ? newValue.id : '')
+                                        }
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="Product"
+                                                variant="outlined"
+                                                error={!!errors[`productId_${index}`]}
+                                                helperText={errors[`productId_${index}`]}
+                                            />
+                                        )}
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item sx={{ width: '100%' }}>
+                                {/* Quantity Input */}
+                                <TextField
+                                    label="Quantity"
+                                    type="number"
+                                    variant="outlined"
+                                    value={product.quantity}
+                                    onChange={(e) =>
+                                        handleProductChange(index, 'quantity', e.target.value)
+                                    }
+                                    error={!!errors[`quantity_${index}`]}
+                                    helperText={errors[`quantity_${index}`]}
+                                />
+                                {/* Remove Button */}
+                                <IconButton color="error" onClick={() => handleRemoveProduct(index)} sx={{pt: 2}} >
+                                    <Remove />
+                                </IconButton>
+                            </Grid>
+                        </Grid>
+                    ))}
+                </Grid>
+
+                    {/* Add Product Button */}
+                    <Grid item xs={12} display="flex" justifyContent="center">
+                        <Button
+                            variant="outlined"
+                            startIcon={<Add />}
+                            onClick={handleAddProduct}
+                            sx={{ mt: 1, px: 4 }}
+                        >
+                            Add Product
+                        </Button>
+                    </Grid>
+
+                    {/* Save Order Button */}
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="primary" fullWidth onClick={handleSubmit}>
+                            Save Sales Order
+                        </Button>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     );
